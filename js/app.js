91b80c414ed938746dd5cff288574c173dcb6394
GraceGenuardi <graziagenuardi96@gmail.com>
2023-02-15 15:59:28 +0100

    FIX problema classe active + Problema display none

--- js/app.js
+++ js/app.js
@@ -1,106 +1,98 @@
-//MILESTONE 2:Al click dell’utente sulle frecce (una a sinistra e una a destra), il programma cambierà l’immagine attiva, che quindi verrà visualizzata al posto della precedente. 
 
 
 
 
-//1. Dichiariamo le constanti slide, arrow e current slide del nostro carosello
+
+// Dichiarazione delle costanti per il carosello
 const slides = document.querySelectorAll('.slide');
 const leftArrow = document.querySelector('.arrow-left');
 const rightArrow = document.querySelector('.arrow-right');
 let currentSlide = 0;
 
-//2. Resetto le slide con for per dare display none
+// Funzione per impostare il display "none" a tutte le slide
 function reset() {
-    for (let i = 0; i < slides.length; i++) {
-      slides[i].style.display = 'none';
-    }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
   }
-
-//3. Diamo display block allo slideshow
-function startSlide() {
-    reset();
-    slides[0].style.display = 'block';
-  }  
-
-
- //4. Preview delle slide
-function prevSlide() {
-    reset();
-    slides[currentSlide - 1].style.display = 'block';
-    currentSlide--;
-  } 
-
-//5. Mostra la prossima slide
-function nextSlide() {
-    reset();
-    slides[currentSlide + 1].style.display = 'block';
-    currentSlide++;
-  }
-  
-  //6. Attiva la freccia sinistra per lo switch
-  leftArrow.addEventListener('click', function() {
-    if (currentSlide === 0) {
-      currentSlide = slides.length;
-    }
-    prevSlide();
-  });
-  
-  //7.Attiva la freccia destra per lo switch
-  rightArrow.addEventListener('click', function() {
-    if (currentSlide === slides.length - 1) {
-      currentSlide = -1;
-    }
-    nextSlide();
-  });
-  
-  startSlide();  
-
-
-//MILESTONE 3
-
-
-//1. Array con img
-const images = [
-    {
-        image: './img/image1.jpg',
-        title: 'Frodo',
-        text: 'Il mio tesoro',
-    }, {
-        image: './img/image2.jpg',
-        title: 'Frodo',
-        text: 'Il mio tesoro',
-    }, {
-        image: './img/image3.jpg',
-        title: 'Frodo',
-        text: 'Il mio tesoro',
-    }, {
-        image: './img/image4.jpg',
-        title: 'Frodo',
-        text: 'Il mio tesoro',
-    }, {
-        image: './img/image5.jpg',
-        title: 'Frodo',
-        text: 'Il mio tesoro',
-    }
-];
-
-  //2. Crea una funzione per generare markup
-
-  function generateImageMarkup(images) {
-    let markup = '';
-    for (let i = 0; i < images.length; i++) {
-      markup += `
-        <div class="slide">
-          <img src="${images[i].image}" alt="${images[i].title}">
-          <h2>${images[i].title}</h2>
-          <p>${images[i].text}</p>
-        </div>
-      `;
-    }
-    return markup;
 }
 
-  //3. Generare generatemarkup
+// Funzione per mostrare una slide specifica
+function showSlide(index) {
+  reset();
+  slides[index].style.display = 'block';
+  currentSlide = index;
+}
 
-  const slidesContainer = document.querySelector('#slides-container');
+// Funzione per mostrare la slide successiva
+function nextSlide() {
+  let newIndex = currentSlide + 1;
+  if (newIndex >= slides.length) {
+    newIndex = 0;
+  }
+  showSlide(newIndex);
+}
+
+// Funzione per mostrare la slide precedente
+function prevSlide() {
+  let newIndex = currentSlide - 1;
+  if (newIndex < 0) {
+    newIndex = slides.length - 1;
+  }
+  showSlide(newIndex);
+}
+
+// Aggiunta degli eventi click sulle frecce
+leftArrow.addEventListener('click', prevSlide);
+rightArrow.addEventListener('click', nextSlide);
+
+// Array di immagini
+const images = [
+  {
+    image: './img/image1.jpg',
+    title: 'Frodo',
+    text: 'Il mio tesoro',
+  },
+  {
+    image: './img/image2.jpg',
+    title: 'Frodo',
+    text: 'Il mio tesoro',
+  },
+  {
+    image: './img/image3.jpg',
+    title: 'Frodo',
+    text: 'Il mio tesoro',
+  },
+  {
+    image: './img/image4.jpg',
+    title: 'Frodo',
+    text: 'Il mio tesoro',
+  },
+  {
+    image: './img/image5.jpg',
+    title: 'Frodo',
+    text: 'Il mio tesoro',
+  },
+];
+
+// Generazione del markup per le slide
+function generateImageMarkup(images) {
+  let markup = '';
+  for (let i = 0; i < images.length; i++) {
+    const activeClass = i === 0 ? 'active' : '';
+    markup += `
+      <div class="slide ${activeClass}">
+        <img src="${images[i].image}" alt="${images[i].title}">
+        <h2>${images[i].title}</h2>
+        <p>${images[i].text}</p>
+      </div>
+    `;
+  }
+  return markup;
+}
+
+// Inserimento delle slide nel carosello
+const slidesContainer = document.querySelector('#slides-container');
 slidesContainer.innerHTML = generateImageMarkup(images);
+
+// Mostrare la prima slide all'avvio
+showSlide(0);
